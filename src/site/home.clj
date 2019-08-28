4a4ec7a8acbd8a647d78a4e73fd2b8f338c54c71
Daw-Ran Liou <dawran6@gmail.com>
2019-08-28 09:20:28 -0700

    Change landing page to todos

--- src/site/home.clj
+++ src/site/home.clj
@@ -3,5 +3,15 @@
 
 
 (defn index [request]
-  [:h1 {:class "tc"}
-   "You're watching clojure/walk!"])
+  (let [rows (coast/q '[:select *
+                        :from todo
+                        :order id desc])]
+    [:div.pa3.measure.center
+     [:h1.ttu.tracked "All todos"]
+     (if (seq rows)
+       [:ul.list.pl0
+        (for [{:todo/keys [name finished]} rows]
+          [:li.lh-copy.pv3.bb.b--black-30
+           {:class (when (= 1 finished) "strike")}
+           name])]
+       [:p "Nothing to do!"])]))
